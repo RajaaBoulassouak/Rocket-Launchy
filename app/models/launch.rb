68cc7c6bf7acdb14b74899eddc5075268752cf1a
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-10-21 11:49:15 -0600

    add modifications

--- app/models/launch.rb
+++ app/models/launch.rb
@@ -9,8 +9,6 @@ class Launch
   def initialize(data)
     @name = data[:name]
     @date = data[:net]
-    @location_name = data[:location][:pads].first[:name]
-    @location_url = data[:location][:pads].first[:mapURL]
     @rocket_name = data[:rocket][:name]
   end
 end 
--- spec/features/user_can_see_launches_index_spec.rb
+++ spec/features/user_can_see_launches_index_spec.rb
@@ -8,8 +8,6 @@ feature 'User can visits launches index page' do
     within(first(".launch")) do    
       expect(page).to have_css(".name")
       expect(page).to have_css(".date")
-      expect(page).to have_css(".location_name")
-      expect(page).to have_css(".location_url")
       expect(page).to have_css(".rocket_name")
     end
   end
